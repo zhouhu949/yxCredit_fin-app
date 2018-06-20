ad98d3c2a85e8ecb729b6d01cfa82fdd76dedb9c
han <812759359@qq.com>
2018-06-20 15:57:14 +0800

    需求更改

--- fin-service/src/main/java/com/zw/rule/activity/service/impl/ActivityServiceImpl.java
+++ fin-service/src/main/java/com/zw/rule/activity/service/impl/ActivityServiceImpl.java
@@ -6,12 +6,8 @@ import com.zw.base.util.StringUtils;
 import com.zw.rule.activity.Activity;
 import com.zw.rule.activity.service.ActivityService;
 import com.zw.rule.mapper.activity.ActivityMapper;
-import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
@@ -116,36 +112,25 @@ public class ActivityServiceImpl implements ActivityService {
      */
     @Override
     public List uploadaBannerImage(HttpServletRequest request) throws Exception {
-        /**
-         * 韩梅生
-         */
-        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-        MultipartFile multipartFile = multipartRequest.getFile("file");
-        multipartFile.transferTo(new File(""));
-        // 获取文件名
-        String fileName = multipartFile.getOriginalFilename();
-        // 获取文件后缀
-        String prefix=fileName.substring(fileName.lastIndexOf("."));
-
-        File file = (File) multipartFile;
-//        String id = GeneratePrimaryKeyUtils.getUUIDKey();//新的文件名
-//        //获取根目录
-//        //String root = request.getSession().getServletContext().getRealPath("/");
-//        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-//        String currentDateStr = format.format(new Date());
-//        String newFilePath = imgPath + File.separator + bannerImg + currentDateStr;//文件保存路径url
-//        Map<String, Object> allMap = UploadFile.getFile(request, newFilePath, id);
-//        List<Map<String, Object>> list = (List<Map<String, Object>>) allMap.get("fileList");
+        String fileName="";
+        String id = GeneratePrimaryKeyUtils.getUUIDKey();//新的文件名
+        //获取根目录
+        //String root = request.getSession().getServletContext().getRealPath("/");
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String currentDateStr = format.format(new Date());
+        String newFilePath = imgPath + File.separator + bannerImg + currentDateStr;//文件保存路径url
+        Map<String, Object> allMap = UploadFile.getFile(request, newFilePath, id);
+        List<Map<String, Object>> list = (List<Map<String, Object>>) allMap.get("fileList");
         //当前台有文件时，给图片名称变量赋值
-//        if (!list.isEmpty()) {
-//            Map<String, Object> fileMap = list.get(0);
-//            fileName = bannerImg + currentDateStr+"/"+ fileMap.get("Name").toString();
-//        }
+        if (!list.isEmpty()) {
+            Map<String, Object> fileMap = list.get(0);
+            fileName = bannerImg + currentDateStr+"/"+ fileMap.get("Name").toString();
+        }
         List imageList = new ArrayList();
         String fileFormat= fileName.substring(fileName.indexOf(".") + 1);
 
         if ("png".equals(fileFormat) || "jpg".equals(fileFormat) || "jpeg".equals(fileFormat) || "gif".equals(fileFormat) || "".equals(fileFormat)){
-            BufferedImage sourceImg= ImageIO.read(file);
+            BufferedImage sourceImg= ImageIO.read(new File(imgPath+fileName));
             Integer imgWidth= sourceImg.getWidth();//750px
             Integer imgHeight=sourceImg.getHeight();//380px
             if (imgWidth==750 && imgHeight ==380){
